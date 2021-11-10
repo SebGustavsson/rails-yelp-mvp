a3f796a70982f966306bddda1642f1dc538d35c3
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-10 15:19:31 +0100

    everything working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,18 @@
 class ReviewsController < ApplicationController
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
-      redirect_to @review.restaurant, notice: "Review was created"
+      redirect_to restaurant_path(@restaurant), notice: "Review was created"
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
