7b82f1b8ceca79deb7dc3be023b9439d16f7bab8
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-10 12:56:58 +0100

    done with backend, now to fix views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,32 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    set_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(require_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def require_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Farang', address: 'Östermalm', category: 'chinese')
+Review.create(content: 'blablablabla', rating: 5, restaurant_id: '1')
